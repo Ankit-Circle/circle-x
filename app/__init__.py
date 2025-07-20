fd4a057a10e44e17c5920e962373ae35ba081b4e
Pranav Acharya <82842289+pranav-loops@users.noreply.github.com>
2025-07-19 23:44:08 -0700

    add cors to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)  # Enable CORS for all routes
 
     # Import Blueprints
     from app.image_enhancement import image_enhancement_bp
